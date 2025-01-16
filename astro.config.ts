6218c68fcde97d91905ad245b6b054d267e15126
indra87g <twinightwheel50@gmail.com>
2025-01-16 23:18:59 +0700

    feat(main): add vercel insights

--- astro.config.ts
+++ astro.config.ts
@@ -5,25 +5,28 @@ import expressiveCode from 'astro-expressive-code'
 
 import mdx from '@astrojs/mdx'
 
+import icon from 'astro-icon';
+
+import alpinejs from '@astrojs/alpinejs';
+
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    react(),
-    expressiveCode({
-      themes: ['github-dark', 'github-light'],
-      styleOverrides: {
-        frames: {
-          editorActiveTabIndicatorTopColor: 'transparent',
-          editorActiveTabBorderColor: '#80808080',
-          editorTabBarBorderBottomColor: '#80808080',
-          tooltipSuccessBackground: 'black',
-        },
-        uiFontFamily: 'inherit',
-        borderColor: '#80808080',
+  integrations: [tailwind(), react(), expressiveCode({
+    themes: ['github-dark', 'github-light'],
+    styleOverrides: {
+      frames: {
+        editorActiveTabIndicatorTopColor: 'transparent',
+        editorActiveTabBorderColor: '#80808080',
+        editorTabBarBorderBottomColor: '#80808080',
+        tooltipSuccessBackground: 'black',
       },
-    }),
-    mdx(),
-  ],
-  site: 'https://neobrutalism-blog.netlify.app/',
+      uiFontFamily: 'inherit',
+      borderColor: '#80808080',
+    },
+  }), mdx(), icon(), alpinejs()],
+
+  site: 'https://indra87g.surge.sh/',
+  adapter: vercel(),
 })
