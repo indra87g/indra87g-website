d48a8f7fe6cc0bb54de7d16be540b006c3a0ee16
indra87g <twinightwheel50@gmail.com>
2025-01-17 22:30:43 +0700

    f

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,12 +8,6 @@ const blogCollection = defineCollection({
     pubDate: z.string().or(z.date()),
     tags: z.array(z.string()),
   }),
-  slug: ({ id }) => {
-    if (typeof id === 'string') {
-      return id.split('/').pop()?.replace('.md', '') || '';
-    }
-    return '';
-  },
 });
 
 export const collections = {
