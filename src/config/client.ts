13739f943d4a2a9f1ccc3cc881baa4a4e3dbbd67
indra87g <twinightwheel50@gmail.com>
2025-01-17 19:32:46 +0700

    feat(main): add vercel insights

---
+++ src/config/client.ts
@@ -0,0 +1,16 @@
+//import { SITE_URL } from 'astro:env/client'
+
+export const CONFIG_CLIENT = {
+    SITE_URL: '',
+    SITE_TITLE: 'indra87g Personal Website',
+    SITE_DESCRIPTION: '',
+    AUTHOR_EMAIL: 'noeldycreator@gmail.com',
+    AUTHOR_FACEBOOK: 'https://facebook.com/indra87g',
+    AUTHOR_LINKEDIN: 'https://linkedin.com',
+    AUTHOR_BLUESKY: 'https://indra87g.bluesky.social',
+    AUTHOR_YOUTUBE: 'https://youtube.com/@indra87g30spki',
+    AUTHOR_GITHUB: 'https://github.com/indra87g',
+    AUTHOR_WHATSAPP: 'https://wa.me/6289530428615',
+    AUTHOR_WHATSAPP_CH: '',
+    AUTHOR_TRAKTEER: 'https://trakteer.id/nightw5050',
+}
